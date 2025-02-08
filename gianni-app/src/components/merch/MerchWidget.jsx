696f835615475caa5c33348abf4c7ba6d706c1bb
szilagyiZoltan0 <zoltan.szilagyi@devconsult.hu>
2025-02-08 20:52:49 +0100

    merch list az order page  fronted+backend connection

--- gianni-app/src/components/merch/MerchWidget.jsx
+++ gianni-app/src/components/merch/MerchWidget.jsx
@@ -1,11 +1,24 @@
-/* eslint-disable no-empty-pattern */
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PrimaryBtn from '../buttons/PrimaryBtn'
 import MerchWidgetItem from './MerchWidgetItem'
 
-const MerchWidget = ({  }) => {
+const MerchWidget = () => {
+  const [merchItems, setMerchItems] = useState([]);
+
+  useEffect(() => {
+    const fetchMerchItems = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/merch');
+        const data = await response.json();
+        setMerchItems(data);
+      } catch (error) {
+        console.log('Error fetching merch items:', error);
+      }
+    };
+
+    fetchMerchItems();
+  }, []);
+
   return (
     <div className='w-full h-[550px] flex flex-col justify-start items-center bg-slate-600 font-poppins rounded-[26px] shadow-black/50 shadow-2xl duration-500 pt-8 gap-2'>
         <h2 className='text-2xl font-bold text-center text-light'>
@@ -13,12 +26,14 @@ const MerchWidget = ({  }) => {
         </h2>
 
         <div className='w-full flex flex-col justify-start items-center px-4 overflow-auto mb-6'>
-          <MerchWidgetItem name="Hoodie" price="7000"/>
-          <MerchWidgetItem name="Keychain" price="2000"/>
-          <MerchWidgetItem name="Hoodie" price="7000"/>
-          <MerchWidgetItem name="Keychain" price="2000"/>
+          {merchItems.map((item) => (
+            <MerchWidgetItem 
+              key={item._id}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
         </div>
-
     </div>
   )
 }
