42d4a56c385d45bef2f65f214f9a30710faf0955
Levente Nagy <bbarbi2002316@gmail.com>
2025-02-17 11:43:44 +0100

    Added translation support for "merches" string

--- gianni-app/src/components/merch/MerchWidget.jsx
+++ gianni-app/src/components/merch/MerchWidget.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import MerchWidgetItem from './MerchWidgetItem'
+import { useTranslation } from 'react-i18next';
 
 const MerchWidget = () => {
   const [merchItems, setMerchItems] = useState([]);
+  const { t } = useTranslation();
 
   useEffect(() => {
     const fetchMerchItems = async () => {
@@ -21,7 +23,7 @@ const MerchWidget = () => {
   return (
     <div className='w-full h-[550px] flex flex-col justify-start items-center bg-slate-600 font-poppins rounded-[26px] shadow-black/50 shadow-2xl duration-500 pt-8 gap-2'>
         <h2 className='text-2xl font-bold text-center text-light'>
-            Merches
+            {t("merches")}
         </h2>
 
         <div className='w-full flex flex-col justify-start items-center px-4 overflow-auto mb-6'>
