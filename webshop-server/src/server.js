8505afdcc68cea353a8615a530fd73f0e0ae37cb
szilagyiZoltan0 <zoltan.szilagyi@devconsult.hu>
2025-02-24 14:46:10 +0100

    prod fix

--- webshop-server/src/server.js
+++ webshop-server/src/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({
 }));
 
 app.use(cors({
-    origin: ['http://91.214.112.140:3002', 'http://localhost:5173'],
+    origin: ['http://91.214.112.140:80', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
