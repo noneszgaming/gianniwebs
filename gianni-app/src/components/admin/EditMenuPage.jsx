95e0eac9c80d1f95b3113c007caa63a05c72ac0a
szilagyiZoltan0 <zoltan.szilagyi@devconsult.hu>
2025-02-26 10:39:44 +0100

    allergen edit oldal create delete

--- gianni-app/src/components/admin/EditMenuPage.jsx
+++ gianni-app/src/components/admin/EditMenuPage.jsx
@@ -12,7 +12,39 @@ const EditMenuPage = () => {
     useSignals();
     const [foods, setFoods] = useState([]);
     const [merches, setMerches] = useState([]);
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    const [allergenes, setAllergenes] = useState([]);
+
+    const [allergeneInput, setAllergeneInput] = useState({
+        hu: '',
+        en: '',
+        de: ''
+    });
+
+
+    const handleAllergeneSubmit = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/specialtypes`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('adminToken')}`
+                },
+                body: JSON.stringify({
+                    name: allergeneInput
+                })
+            });
+
+            if (response.ok) {
+                // Clear input after successful submission
+                setAllergeneInput({ hu: '', en: '', de: '' });
+                isAddAllergeneOpened.value = false;
+                fetchAllergenes();
+            }
+        } catch (error) {
+            console.error('Error submitting allergene:', error);
+        }
+    };
 
     const fetchItems = async () => {
         try {
@@ -22,16 +54,43 @@ const EditMenuPage = () => {
                 }
             });
             const data = await response.json();
-            
+
             setFoods(data.filter(item => item.type === 'food'));
             setMerches(data.filter(item => item.type === 'merch'));
         } catch (error) {
             console.log('Error fetching items:', error);
         }
     };
+    const fetchAllergenes = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/public/specialtypes`);
+            const data = await response.json();
+            setAllergenes(data);
+        } catch (error) {
+            console.error('Error fetching allergenes:', error);
+        }
+    };
+    const deleteAllergene = async (id) => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/specialtypes/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('adminToken')}`
+                }
+            });
+
+            if (response.ok) {
+                fetchAllergenes();
+            }
+        } catch (error) {
+            console.error('Error deleting allergene:', error);
+        }
+    };
+
 
     useEffect(() => {
         fetchItems();
+        fetchAllergenes();
     }, []);
 
     return (
@@ -76,27 +135,60 @@ const EditMenuPage = () => {
                 <div className='bg-light w-full h-fit flex flex-col justify-center items-center gap-4 rounded-[30px] px-4 pt-2 pb-4 shadow-black/50 shadow-2xl'>
                     <div className='w-full h-fit flex justify-center items-center gap-2'>
                         <h2 className='text-xl font-bold text-dark self-center'>{t("allergens.title")}</h2>
-                        <button 
+                        <button
                             className='w-8 aspect-square bg-accent hover:bg-dark-accent rounded-[8px] flex justify-center items-center duration-500 cursor-pointer'
-                            onClick={() => {isAddAllergeneOpened.value = !isAddAllergeneOpened.value;}}
+                            onClick={() => { isAddAllergeneOpened.value = !isAddAllergeneOpened.value; }}
                         >
                             <IoIosAdd className={`w-8 h-8 text-light transition-transform duration-500 ${isAddAllergeneOpened.value ? 'rotate-45' : ''}`} />
                         </button>
                     </div>
                     {isAddAllergeneOpened.value && (
-                        <div className='w-full h-fit flex justify-center items-center gap-2'>
-                            <input 
-                                type="text" 
-                                className='w-full h-10 px-2 bg-light border-2 border-dark focus:border-accent rounded-[8px] outline-none caret-accent focus:text-accent' 
-                                placeholder={t("allergens.placeholder")} 
+                        <div className='w-full h-fit flex flex-col gap-2'>
+                            <input
+                                type="text"
+                                value={allergeneInput.hu}
+                                onChange={(e) => setAllergeneInput({ ...allergeneInput, hu: e.target.value })}
+                                className='w-full h-10 px-2 bg-light border-2 border-dark focus:border-accent rounded-[8px] outline-none caret-accent focus:text-accent'
+                                placeholder="Magyar név"
                             />
-                            <button className='w-10 aspect-square bg-accent hover:bg-dark-accent rounded-[8px] flex justify-center items-center duration-500 cursor-pointer'>
+                            <input
+                                type="text"
+                                value={allergeneInput.en}
+                                onChange={(e) => setAllergeneInput({ ...allergeneInput, en: e.target.value })}
+                                className='w-full h-10 px-2 bg-light border-2 border-dark focus:border-accent rounded-[8px] outline-none caret-accent focus:text-accent'
+                                placeholder="English name"
+                            />
+                            <input
+                                type="text"
+                                value={allergeneInput.de}
+                                onChange={(e) => setAllergeneInput({ ...allergeneInput, de: e.target.value })}
+                                className='w-full h-10 px-2 bg-light border-2 border-dark focus:border-accent rounded-[8px] outline-none caret-accent focus:text-accent'
+                                placeholder="Deutsche name"
+                            />
+                            <button
+                                onClick={handleAllergeneSubmit}
+                                className='w-10 aspect-square bg-accent hover:bg-dark-accent rounded-[8px] flex justify-center items-center duration-500 cursor-pointer'
+                            >
                                 <MdSubdirectoryArrowLeft className='w-6 h-6 text-light' />
                             </button>
                         </div>
                     )}
                     <div className='w-full h-fit flex flex-col justify-items-center items-start gap-2'>
-                        <h2 className={`text-md text-dark self-center`}>{t("allergens.no-allergens")}</h2>
+                        {allergenes.length === 0 ? (
+                            <h2 className={`text-md text-dark self-center`}>{t("allergens.no-allergens")}</h2>
+                        ) : (
+                            allergenes.map((allergene) => (
+                                <div key={allergene.id} className="w-full flex justify-between items-center p-2 border-b border-dark">
+                                    <span>{allergene.name[i18n.language]}</span>
+                                    <button
+                                        onClick={() => deleteAllergene(allergene.id)}
+                                        className="p-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
